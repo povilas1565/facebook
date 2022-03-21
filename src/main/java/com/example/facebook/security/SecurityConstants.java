ef746991d05a371ffcc3bfde0cb7a2d515f6229a
paul156551 <pavel.ryzhkov.99@mail.ru>
2022-03-21 17:07:14 +0300

    Проект

--- src/main/java/com/example/facebook/security/SecurityConstants.java
+++ src/main/java/com/example/facebook/security/SecurityConstants.java
@@ -1,7 +1,7 @@
 package com.example.facebook.security;
 
 public class SecurityConstants {
-        public static final String SIGN_UP_URLS = "api/auth/**";
+        public static final String SIGN_UP_URLS = "/api/auth/**";
         public static final String SECRET_KEY = "dog";
         public static final String TOKEN_PREFIX= "Bearer";
         public static final String HEADER_STRING= "Authorization";
